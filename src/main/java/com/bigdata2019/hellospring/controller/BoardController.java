77db800220cdcfb936d84bbf9735f17856b9cfb7
donghyun <hyunlove12@naver.com>
2019-12-26 14:52:38 +0900

    주석 추가

--- src/main/java/com/bigdata2019/hellospring/controller/BoardController.java
+++ src/main/java/com/bigdata2019/hellospring/controller/BoardController.java
@@ -2,11 +2,17 @@ package com.bigdata2019.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequestMapping("/board")
 @Controller
 public class BoardController {
+	/*
+	 * restful
+	 * method=RequestMethod.GET/POST/PUT/DELETE ->
+	 *   
+	 * */
 	
 	@ResponseBody
 	@RequestMapping("/list")
@@ -17,7 +23,7 @@ public class BoardController {
 	}
 	
 	@ResponseBody
-	@RequestMapping("/writeform")
+	@RequestMapping(value = "/writeform", method=RequestMethod.GET)
 	public String writeform() {
 		//responseBody -> /WEB-INF/views/list.jsp리턴 값이 그대로 보여진다. 뷰 리졸버가 아닌 메시지 컨버터가 처리한다. 
 		//responsebody에 직접 값을 전달하겠다.
