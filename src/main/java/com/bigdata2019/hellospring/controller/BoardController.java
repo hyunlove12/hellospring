e25754cd00d393c1e9b4516ef9e03178626e951f
donghyun <hyunlove12@naver.com>
2019-12-26 13:55:31 +0900

    설정 추가

---
+++ src/main/java/com/bigdata2019/hellospring/controller/BoardController.java
@@ -0,0 +1,34 @@
+package com.bigdata2019.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/board")
+@Controller
+public class BoardController {
+	
+	@ResponseBody
+	@RequestMapping("/list")
+	public String list() {
+		//responseBody -> /WEB-INF/views/list.jsp리턴 값이 그대로 보여진다. 뷰 리졸버가 아닌 메시지 컨버터가 처리한다. 
+		//responsebody에 직접 값을 전달하겠다.
+		return "board:list";
+	}
+	
+	@ResponseBody
+	@RequestMapping("/writeform")
+	public String writeform() {
+		//responseBody -> /WEB-INF/views/list.jsp리턴 값이 그대로 보여진다. 뷰 리졸버가 아닌 메시지 컨버터가 처리한다. 
+		//responsebody에 직접 값을 전달하겠다.
+		return "board:writeform";
+	}
+		
+	@RequestMapping("/write")
+	public String write() {
+		System.out.println("board:write");
+		//contextpath제외
+		return "redirect:/board/list";
+	}
+	
+}
--- src/main/java/com/bigdata2019/hellospring/controller/HelloController.java
+++ src/main/java/com/bigdata2019/hellospring/controller/HelloController.java
@@ -2,14 +2,18 @@ package com.bigdata2019.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 	
-	@RequestMapping("/hello")
-	public String hello(String name) {
-		
+	//@RequestParam 생략가능
+	//(value="n") 변수명이 n
+	@RequestMapping("/hello")	
+	public String hello( @RequestParam(value="n", required=true, defaultValue="") String name
+			           , @RequestParam(value="a", required=true, defaultValue="0") int age) {		
 		System.out.println("name : " + name);
+		System.out.println("age : "  + age);
 		//root경로는 webapp
 		//jsp로 포워딩
 		return "/WEB-INF/views/hello.jsp";
